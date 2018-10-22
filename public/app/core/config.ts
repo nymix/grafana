bd8b4eb6596d3abb2781a1e0d4154e4c9a90c388
Daniel Povey <dpovey96@gmail.com>
2018-10-22 13:42:06 +0100

    change images from grafana brand

--- public/app/core/config.ts
+++ public/app/core/config.ts
@@ -36,7 +36,7 @@ export class Settings {
   constructor(options) {
     const defaults = {
       datasources: {},
-      windowTitlePrefix: 'Grafana - ',
+      windowTitlePrefix: 'Data - ',
       panels: {},
       newPanelTitle: 'Panel Title',
       playlist_timespan: '1m',
